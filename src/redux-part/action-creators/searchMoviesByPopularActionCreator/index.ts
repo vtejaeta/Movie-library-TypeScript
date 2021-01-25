27f54793c7837a43204e25893fb1a6d9c5da0f37
vtejaeta <vtejaeta@gmail.com>
2021-01-25 18:45:59 +0530

    Setup of search movie by popular action creator

--- src/redux-part/action-creators/searchMoviesByPopularActionCreator/index.ts
+++ src/redux-part/action-creators/searchMoviesByPopularActionCreator/index.ts
@@ -1,24 +1,26 @@
-import axios from 'axios'
 import { SearchMoviesByPopularActionType } from '../../action-types/searchMoviesByPopular'
 import { SearchMoviesByPopularAction } from '../../actions/searchMoviesByPopular'
 import GetMovies from '../../../api/GetMovies'
 
-const searchMoviesByPopular = (pageId:number) => {
+export const searchMoviesByPopular = (pageId: number) => {
   return async (dispatch: any) => {
     dispatch({
       type: SearchMoviesByPopularActionType.SEARCH_MOVIES_BY_POPULAR,
     })
 
     try {
-        const {data} = await GetMovies.get('/movie/popular',{
-            params:{
-                api_key: process.env.REACT_APP_API_KEY,
-                language: "en-US",
-                page: pageId
-            }
-        })
+      const { data } = await GetMovies.get('/movie/popular', {
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+          language: 'en-US',
+          page: pageId,
+        },
+      })
 
-        
+      dispatch({
+        type: SearchMoviesByPopularActionType.SEARCH_MOVIES_BY_POPULAR_SUCCESS,
+        payload: data,
+      })
     } catch (error) {
       dispatch({
         type: SearchMoviesByPopularActionType.SEARCH_MOVIES_BY_POPULAR_ERROR,
