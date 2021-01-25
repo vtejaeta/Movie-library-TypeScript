66bc8382f3f05f4c071bee7a5c373ea58a984c13
vtejaeta <vtejaeta@gmail.com>
2021-01-25 14:16:13 +0530

    Initial App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-function App() {
-  return <div className='App'>Hello</div>
+const App = () => {
+  return (
+    <>
+      <h1>Navbar</h1>
+      <h1>Carousel</h1>
+    </>
+  )
 }
 
 export default App
