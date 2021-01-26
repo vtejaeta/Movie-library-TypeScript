5302aaedead438d097b7c5994a1731fd7dbbeb8f
vtejaeta <vtejaeta@gmail.com>
2021-01-26 11:14:37 +0530

    setup of return type of combine reducers

--- src/components/MoviesListByPopular.tsx
+++ src/components/MoviesListByPopular.tsx
@@ -1,13 +1,18 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 import { useState, useEffect } from 'react'
 import { useActions } from '../hooks/useActions'
 
 const MoviesListByPopular: React.FC = () => {
-  const [movies, setMovies] = useState({})
+  const [movies, setMovies] = useState<{
+    page: number
+    results: []
+    total_pages: number
+  } | {}>({})
   const { searchMoviesByTopRated, searchMoviesByPopular } = useActions()
-  
+  const {data,error,loading} = useSelector(state => state.moviesByPopular)
+
   useEffect(() => {
-      searchMoviesByPopular(1)
+    searchMoviesByPopular(1)
   }, [])
 
   return (
--- src/redux-part/exports.ts
+++ src/redux-part/exports.ts
@@ -1,3 +1,4 @@
 export * from './store'
 export { searchMoviesByPopular } from './action-creators/searchMoviesByPopularActionCreator'
 export { searchMoviesByTopRated } from './action-creators/searchMoviesByTopRatedActionCreator'
+export * from './reducers'
