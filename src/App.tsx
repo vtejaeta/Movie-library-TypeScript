510c08406e6a0547b8f5915a84f1000db749eaf1
vtejaeta <vtejaeta@gmail.com>
2021-01-26 10:22:19 +0530

    Refactor of app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { Provider } from 'react-redux'
-import { store } from './redux-part/exports'
 import MoviesList from './components/MoviesList'
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <>
       <h1>Navbar</h1>
       <h1>Carousel</h1>
-      <MoviesList/>
-    </Provider>
+      <MoviesList />
+    </>
   )
 }
 
