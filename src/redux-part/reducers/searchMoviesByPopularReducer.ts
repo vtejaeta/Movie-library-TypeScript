4651f36b8ab1607bd361006a708fd186d594da46
vtejaeta <vtejaeta@gmail.com>
2021-01-26 11:25:33 +0530

    Setup of type of empty result object

--- src/redux-part/reducers/searchMoviesByPopularReducer.ts
+++ src/redux-part/reducers/searchMoviesByPopularReducer.ts
@@ -8,11 +8,11 @@ const searchMoviesByPopularReducer = (
 ): MoviesState => {
   switch (action.type) {
     case SearchMoviesByPopularActionType.SEARCH_MOVIES_BY_POPULAR:
-      return { loading: true, error: null, data: [] }
+      return { loading: true, error: null, data: {} }
     case SearchMoviesByPopularActionType.SEARCH_MOVIES_BY_POPULAR_SUCCESS:
       return { loading: false, error: null, data: action.payload }
     case SearchMoviesByPopularActionType.SEARCH_MOVIES_BY_POPULAR_ERROR:
-      return { loading: false, error: action.payload, data: [] }
+      return { loading: false, error: action.payload, data: {} }
     default:
       return state
   }
