7192173cbaf89e1353b806146b7f89dffb77173e
vtejaeta <vtejaeta@gmail.com>
2021-01-28 15:45:00 +0530

    Setup of searchMoviesByUpComingActionType

---
+++ src/redux-part/action-types/searchMoviesByUpComing/index.ts
@@ -0,0 +1,6 @@
+export enum SearchMoviesByUpComingActionType {
+    SEARCH_MOVIES_BY_UPCOMING = 'search_movies_by_upcoming',
+    SEARCH_MOVIES_BY_UPCOMING_SUCCESS = 'search_movies_by_upcoming_success',
+    SEARCH_MOVIES_BY_UPCOMING_ERROR = 'search_movies_by_upcoming_error',
+  }
+  
