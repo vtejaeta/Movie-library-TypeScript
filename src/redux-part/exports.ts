ee938d1d9a2d0eb9fb30ae59399dc015e6cebed1
vtejaeta <vtejaeta@gmail.com>
2021-03-13 16:21:04 +0530

    Add getGenreId to redux exports

--- src/redux-part/exports.ts
+++ src/redux-part/exports.ts
@@ -1,6 +1,7 @@
-export * from './store'
-export { searchMoviesByPopular } from './action-creators/searchMoviesByPopularActionCreator'
-export { searchMoviesByTopRated } from './action-creators/searchMoviesByTopRatedActionCreator'
-export { searchMoviesByUpComing } from './action-creators/searchMoviesByUpComingActionCreator'
-export * from './reducers'
-export * as state from './moviesState'
+export * from "./store";
+export { searchMoviesByPopular } from "./action-creators/searchMoviesByPopularActionCreator";
+export { searchMoviesByTopRated } from "./action-creators/searchMoviesByTopRatedActionCreator";
+export { searchMoviesByUpComing } from "./action-creators/searchMoviesByUpComingActionCreator";
+export { getGenreId } from "./action-creators/getGenreIdActionCreator";
+export * from "./reducers";
+export * as state from "./moviesState";
