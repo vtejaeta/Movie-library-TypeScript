17fa47d0d08557ea53fdc9c81b9e86ee5a75a3ae
vtejaeta <vtejaeta@gmail.com>
2021-03-13 18:10:36 +0530

    Add action type for searchMoviesByGenreId

---
+++ src/redux-part/action-types/searchMoviesByGenreId/index.ts
@@ -0,0 +1,6 @@
+export enum SearchMoviesByGenreIdActionType {
+    SEARCH_MOVIES_BY_GENRE_ID = 'search_movies_by_genre_id',
+    SEARCH_MOVIES_BY_GENRE_ID_SUCCESS = 'search_movies_by_genre_id_success',
+    SEARCH_MOVIES_BY_GENRE_ID_ERROR = 'search_movies_by_genre_id_error',
+  }
+  
