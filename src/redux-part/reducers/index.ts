59729432432f7c5a624c771c5a8f86632ee6729a
vtejaeta <vtejaeta@gmail.com>
2021-03-13 16:13:59 +0530

    Add getGenreId in combineReducers

--- src/redux-part/reducers/index.ts
+++ src/redux-part/reducers/index.ts
@@ -1,14 +1,16 @@
-import { combineReducers } from 'redux'
-import searchMoviesByPopularReducer from './searchMoviesByPopularReducer'
-import searchMoviesByTopRatedReducer from './searchMoviesByTopRatedReducer'
-import searchMoviesByUpComingReducer from './searchMoviesByUpComingReducer'
+import { combineReducers } from "redux";
+import getGenreIdReducer from "./getGenreIdReducer";
+import searchMoviesByPopularReducer from "./searchMoviesByPopularReducer";
+import searchMoviesByTopRatedReducer from "./searchMoviesByTopRatedReducer";
+import searchMoviesByUpComingReducer from "./searchMoviesByUpComingReducer";
 
 const reducers = combineReducers({
   moviesByPopular: searchMoviesByPopularReducer,
   moviesByTopRated: searchMoviesByTopRatedReducer,
   moviesByUpComing: searchMoviesByUpComingReducer,
-})
+  getGenreId: getGenreIdReducer,
+});
 
-export default reducers
+export default reducers;
 
-export type RootState = ReturnType<typeof reducers>
+export type RootState = ReturnType<typeof reducers>;
