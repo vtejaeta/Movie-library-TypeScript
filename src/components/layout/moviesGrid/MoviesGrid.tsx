0f89b278f7c5a8ded4758ba1b145ead0ad5f3ffd
vtejaeta <vtejaeta@gmail.com>
2021-03-13 16:28:54 +0530

    Refactor of code to use custom hook

--- src/components/layout/moviesGrid/MoviesGrid.tsx
+++ src/components/layout/moviesGrid/MoviesGrid.tsx
@@ -1,25 +1,25 @@
-import { useTypedSelector } from '../../../hooks/useTypedSelector'
-import { Col, Card } from 'react-bootstrap'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import { state } from '../../../redux-part/exports'
-import '../../../assets/css/MoviesGrid.css'
-import useAccessMoviesData from '../../../hooks/useAccessMoviesData';
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
+import { Col, Card } from "react-bootstrap";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { state } from "../../../redux-part/exports";
+import "../../../assets/css/MoviesGrid.css";
+import useAccessMoviesData from "../../../hooks/useAccessMoviesData";
 
 interface MoviesGridProps {
-  category?: string
-  loadingStatus: string
+  category?: string;
+  loadingStatus: string;
 }
 
 const defaultProps: MoviesGridProps = {
-  category: 'popular',
-  loadingStatus: 'loading',
-}
+  category: "popular",
+  loadingStatus: "loading",
+};
 
 const MoviesGrid: React.FC<MoviesGridProps> = ({ category, loadingStatus }) => {
-  let {data} = useAccessMoviesData(category)
+  let { data } = useAccessMoviesData(category);
 
   const renderMovies = () => {
-    return loadingStatus === 'loading'
+    return loadingStatus === "loading"
       ? Array(20)
           .fill(1)
           .map(() => {
@@ -27,7 +27,7 @@ const MoviesGrid: React.FC<MoviesGridProps> = ({ category, loadingStatus }) => {
               <Col sm={12} md={6} lg={4} xl={3} key={Math.random()}>
                 <Card className={`my-3 ${loadingStatus}`}></Card>
               </Col>
-            )
+            );
           })
       : data?.results.map((movie: any) => {
           return (
@@ -36,19 +36,19 @@ const MoviesGrid: React.FC<MoviesGridProps> = ({ category, loadingStatus }) => {
                 <LazyLoadImage
                   alt={movie.original_title}
                   src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                  className={'movie-thumbnail'}
+                  className={"movie-thumbnail"}
                   height={350}
                   width={230}
                 />
               </Card>
             </Col>
-          )
-        })
-  }
+          );
+        });
+  };
 
-  return <>{renderMovies()}</>
-}
+  return <>{renderMovies()}</>;
+};
 
-MoviesGrid.defaultProps = defaultProps
+MoviesGrid.defaultProps = defaultProps;
 
-export default MoviesGrid
+export default MoviesGrid;
