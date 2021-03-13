c8fd0ae2e6d66b9dde41f7f3687cc59f3de4c86d
vtejaeta <vtejaeta@gmail.com>
2021-03-13 18:14:23 +0530

    Add type of SearchMoviesByGenreId action

---
+++ src/redux-part/actions/searchMoviesByGenreId/index.ts
@@ -0,0 +1,24 @@
+interface searchMoviesByGenreIdAction {
+    type: 'search_movies_by_genre_id'
+  }
+  
+  interface searchMoviesByGenreIdSuccessAction {
+    type: 'search_movies_by_genre_id_success'
+    payload: {
+      page: number
+      results: []
+      total_pages: number
+      total_results: number
+    }
+  }
+  
+  interface searchMoviesByGenreIdErrorAction {
+    type: 'search_movies_by_genre_id_error'
+    payload: string
+  }
+  
+  export type SearchMoviesByGenreIdAction =
+    | searchMoviesByGenreIdAction
+    | searchMoviesByGenreIdSuccessAction
+    | searchMoviesByGenreIdErrorAction
+  
