0769332c97296c5cfb455eaa1d08d8dcd4503969
vtejaeta <vtejaeta@gmail.com>
2021-03-16 19:33:59 +0530

    Remove genreId action creator

--- src/redux-part/exports.ts
+++ src/redux-part/exports.ts
@@ -2,7 +2,6 @@ export * from "./store";
 export { searchMoviesByPopular } from "./action-creators/searchMoviesByPopularActionCreator";
 export { searchMoviesByTopRated } from "./action-creators/searchMoviesByTopRatedActionCreator";
 export { searchMoviesByUpComing } from "./action-creators/searchMoviesByUpComingActionCreator";
-export { getGenreId } from "./action-creators/getGenreIdActionCreator";
 export { searchMoviesByGenreId } from "./action-creators/searchMoviesByGenreIdActionCreator";
 export * from "./reducers";
 export * as state from "./moviesState";
