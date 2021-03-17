716bb36b3e84633134b8245ed177624918c8894f
vtejaeta <vtejaeta@gmail.com>
2021-03-17 16:49:01 +0530

    Add action type of SearchMoviesByName

---
+++ src/redux-part/action-types/searchMoviesByName/index.ts
@@ -0,0 +1,6 @@
+export enum SearchMoviesByNameActionType {
+    SEARCH_MOVIES_BY_NAME = 'search_movies_by_name',
+    SEARCH_MOVIES_BY_NAME_SUCCESS = 'search_movies_by_name_success',
+    SEARCH_MOVIES_BY_NAME_ERROR = 'search_movies_by_name_error',
+}
+  
