b4449e17bfee1a9ef4751bc6dd8509e341091f4d
vtejaeta <vtejaeta@gmail.com>
2021-03-17 20:00:33 +0530

    Added search form functionality

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Navbar,
   Container,
@@ -5,40 +6,57 @@ import {
   Button,
   Form,
   FormControl,
-} from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
-import '../../../assets/css/Header.css'
-import Example  from '../genreModal/GenreModal'
+} from "react-bootstrap";
+import { useHistory } from "react-router";
+import { LinkContainer } from "react-router-bootstrap";
+import "../../../assets/css/Header.css";
+import Example from "../genreModal/GenreModal";
 
 const Header: React.FC = () => {
+  const [keyword, setKeyword] = useState("");
+  const history = useHistory()
+
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand href='/'>CineBasketry</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">CineBasketry</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mr-auto'>
-              <LinkContainer to='/browse/popular/1'>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <LinkContainer to="/browse/popular/1">
                 <Nav.Link>Popular</Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/browse/top-rated/1'>
+              <LinkContainer to="/browse/top-rated/1">
                 <Nav.Link>Top Rated</Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/browse/upcoming/1'>
+              <LinkContainer to="/browse/upcoming/1">
                 <Nav.Link>Upcoming</Nav.Link>
               </LinkContainer>
-                <Example/>
+              <Example />
             </Nav>
-            <Form inline>
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (keyword.trim()) {
+                  history.push(`/search/${keyword}/1`);
+                } else {
+                  history.push("/browse/error");
+                }
+                setKeyword('')
+              }}
+              inline
+            >
               <FormControl
-                type='text'
-                placeholder='Search'
-                className='mr-sm-2 ml-sm-5'
+                type="text"
+                placeholder="Search Movies"
+                className="mr-sm-2 ml-sm-5"
+                value = {keyword}
+                onChange={(e) => setKeyword(e.target.value)}
               />
-              <Button variant='outline-success' className='p-2'>
+              <Button variant="outline-success" className="p-2" type='submit'>
                 Search
               </Button>
             </Form>
@@ -46,7 +64,7 @@ const Header: React.FC = () => {
         </Container>
       </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
