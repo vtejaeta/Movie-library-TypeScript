88c6ea65349bf88f5624d5c219d31c05a5281e43
vtejaeta <vtejaeta@gmail.com>
2021-03-17 16:51:36 +0530

    Add type of SearchMoviesByNameAction

---
+++ src/redux-part/actions/searchMoviesByName/index.ts
@@ -0,0 +1,24 @@
+interface searchMoviesByNameAction {
+    type: 'search_movies_by_name'
+  }
+  
+  interface searchMoviesByNameSuccessAction {
+    type: 'search_movies_by_name_success'
+    payload: {
+      page: number
+      results: []
+      total_pages: number
+      total_results: number
+    }
+  }
+  
+  interface searchMoviesByNameErrorAction {
+    type: 'search_movies_by_name_error'
+    payload: string
+  }
+  
+  export type SearchMoviesByNameAction =
+    | searchMoviesByNameAction
+    | searchMoviesByNameSuccessAction
+    | searchMoviesByNameErrorAction
+  
