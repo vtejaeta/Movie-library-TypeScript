dbdb1568279a2917e46f0fe9a6465e865309f3dd
vtejaeta <vtejaeta@gmail.com>
2021-03-19 10:40:46 +0530

    Added type for results data

---
+++ src/redux-part/actions/searchMoviesById/index.ts
@@ -0,0 +1,38 @@
+interface searchMoviesByIdAction {
+  type: "search_movies_by_id";
+}
+
+interface searchMoviesByIdSuccessAction {
+  type: "search_movies_by_id_success";
+  payload: {
+    genres: { id: Number; name: String }[];
+    id: Number;
+    original_title: String;
+    overview: String;
+    poster_path: String;
+    release_date: String;
+    runtime: Number;
+    videos: {
+      results: {
+        id: String;
+        iso_639_1: String;
+        iso_3166_1: String;
+        key: String;
+        name: String;
+        site: String;
+        size: Number;
+        type: String;
+      }[];
+    };
+  };
+}
+
+interface searchMoviesByIdErrorAction {
+  type: "search_movies_by_id_error";
+  payload: String;
+}
+
+export type SearchMoviesByIdAction =
+  | searchMoviesByIdAction
+  | searchMoviesByIdSuccessAction
+  | searchMoviesByIdErrorAction;
