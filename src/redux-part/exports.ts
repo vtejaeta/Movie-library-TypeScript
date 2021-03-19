ebd0919a0da004b4ecc28bd524ef7e5481e2d9e5
vtejaeta <vtejaeta@gmail.com>
2021-03-19 10:46:52 +0530

    Added action creator to redux exports file

--- src/redux-part/exports.ts
+++ src/redux-part/exports.ts
@@ -4,5 +4,6 @@ export { searchMoviesByTopRated } from "./action-creators/searchMoviesByTopRated
 export { searchMoviesByUpComing } from "./action-creators/searchMoviesByUpComingActionCreator";
 export { searchMoviesByName } from "./action-creators/searchMoviesByNameActionCreator";
 export { searchMoviesByGenreId } from "./action-creators/searchMoviesByGenreIdActionCreator";
+export { searchMoviesById } from "./action-creators/searchMoviesByIdActionCreator";
 export * from "./reducers";
 export * as state from "./moviesState";
