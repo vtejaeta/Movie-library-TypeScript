45acd55af575b3af3309ebf972019d24a40d3100
vtejaeta <vtejaeta@gmail.com>
2021-03-19 11:14:45 +0530

    Added state for movie data

--- src/redux-part/moviesState.ts
+++ src/redux-part/moviesState.ts
@@ -13,3 +13,29 @@ export const initialState = {
   error: null,
   data: null,
 }
+
+export interface MovieState{
+  loading: boolean
+  error: string | null
+  data:{
+    genres: { id: Number; name: String }[];
+    id: Number;
+    original_title: String;
+    overview: String;
+    poster_path: String;
+    release_date: String;
+    runtime: Number;
+    videos: {
+      results: {
+        id: String;
+        iso_639_1: String;
+        iso_3166_1: String;
+        key: String;
+        name: String;
+        site: String;
+        size: Number;
+        type: String;
+      }[];
+    };
+  } | null
+}
