70a2e2a30ea6d9bd7e9b8f1034c40a5a7b166caf
vtejaeta <vtejaeta@gmail.com>
2021-03-19 09:45:28 +0530

    Added type for constants

---
+++ src/redux-part/action-types/searchMoviesById/index.ts
@@ -0,0 +1,6 @@
+export enum SearchMoviesByIdActionType {
+    SEARCH_MOVIES_BY_ID = 'search_movies_by_id',
+    SEARCH_MOVIES_BY_ID_SUCCESS = 'search_movies_by_id_success',
+    SEARCH_MOVIES_BY_ID_ERROR = 'search_movies_by_id_error',
+}
+  
