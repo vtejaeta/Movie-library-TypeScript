f4fb8fd8ea9886a46a385e6f89807d3873b26802
vtejaeta <vtejaeta@gmail.com>
2021-03-20 18:59:27 +0530

    changed types from non primitive to primitive types

--- src/redux-part/moviesState.ts
+++ src/redux-part/moviesState.ts
@@ -18,23 +18,23 @@ export interface MovieState{
   loading: boolean
   error: string | null
   data:{
-    genres: { id: Number; name: String }[];
-    id: Number;
-    original_title: String;
-    overview: String;
-    poster_path: String;
-    release_date: String;
-    runtime: Number;
+    genres: { id: number; name: string }[];
+    id: number;
+    original_title: string;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    runtime: number;
     videos: {
       results: {
-        id: String;
-        iso_639_1: String;
-        iso_3166_1: String;
-        key: String;
-        name: String;
-        site: String;
-        size: Number;
-        type: String;
+        id: string;
+        iso_639_1: string;
+        iso_3166_1: string;
+        key: string;
+        name: string;
+        site: string;
+        size: number;
+        type: string;
       }[];
     };
   } | null
