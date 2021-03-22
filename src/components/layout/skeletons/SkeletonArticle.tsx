c9a124502ee1c8534b3e4e0ffc9ce76805680cfe
vtejaeta <vtejaeta@gmail.com>
2021-03-22 20:11:21 +0530

    Added skeleton layout for movie details screen

--- src/components/layout/skeletons/SkeletonArticle.tsx
+++ src/components/layout/skeletons/SkeletonArticle.tsx
@@ -1,14 +1,24 @@
 import SkeletonElement from "./SkeletonElement";
 
-const SkeletonArticle:React.FC = () => {
-    return <div className='skeleton-wrapper'>
-        <div className='skeleton-article'>
-            <SkeletonElement type="title"/>
-            <SkeletonElement type="text"/>
-            <SkeletonElement type="text"/>
-            <SkeletonElement type="text"/>
+const SkeletonArticle: React.FC = () => {
+  return (
+    <div className="skeleton-wrapper">
+      <div className="skeleton-article">
+        <SkeletonElement type="title" />
+        <div className="skeleton-details">
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
         </div>
+        <div className="skeleton-paragraph">
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
-export default SkeletonArticle
+export default SkeletonArticle;
