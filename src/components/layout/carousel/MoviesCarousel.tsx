e787e14c423d27dd9fdc29e560b7c8e554955558
vtejaeta <vtejaeta@gmail.com>
2021-03-25 20:19:05 +0530

    Add type for movie results

--- src/components/layout/carousel/MoviesCarousel.tsx
+++ src/components/layout/carousel/MoviesCarousel.tsx
@@ -1,41 +1,46 @@
-import '../../../assets/css/MoviesCarousel.css'
-import { Carousel,Image } from 'react-bootstrap'
+import "../../../assets/css/MoviesCarousel.css";
+import { Carousel, Image } from "react-bootstrap";
+import { MoviesResultState } from "../../../redux-part/moviesState";
 
 interface TopFourMovies {
-  topFourMovies?: { [key: string]: any }[]
-  loadingStatus: string
+  topFourMovies?: MoviesResultState[];
+  loadingStatus: string;
 }
 
 const MoviesCarousel: React.FC<TopFourMovies> = ({
   topFourMovies,
   loadingStatus,
 }) => {
+  
   const renderCarousel = () => {
     return (
-      <Carousel className='bg-dark'>
-        {loadingStatus === 'loaded' ? (
-          topFourMovies?.map((movie) => {
-            return (
-              <Carousel.Item key={movie.id}>
-                <Image
-                  alt={movie.original_title}
-                  src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                  className={'movie-thumbnail'}
-                />
-              </Carousel.Item>
-            )
-          })
-        ) : (
-          Array(4).fill(1).map(()=>{
-            return <Carousel.Item key={Math.random()} className='loading'>
-              <div className="dummy-carousel-image"></div>
-            </Carousel.Item>
-          })
-        )}
+      <Carousel className="bg-dark">
+        {loadingStatus === "loaded"
+          ? topFourMovies?.map((movie) => {
+              return (
+                <Carousel.Item key={movie.id}>
+                  <Image
+                    alt={movie.original_title}
+                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                    className={"movie-thumbnail"}
+                  />
+                </Carousel.Item>
+              );
+            })
+          : Array(4)
+              .fill(1)
+              .map(() => {
+                return (
+                  <Carousel.Item key={Math.random()} className="loading">
+                    <div className="dummy-carousel-image"></div>
+                  </Carousel.Item>
+                );
+              })}
       </Carousel>
-    )
-  }
-  return <header>{renderCarousel()}</header>
-}
+    );
+  };
 
-export default MoviesCarousel
+  return <header>{renderCarousel()}</header>;
+};
+
+export default MoviesCarousel;
