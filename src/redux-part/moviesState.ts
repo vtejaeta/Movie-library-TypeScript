c9e73753ea652a450b52d5f618cdd02dfae18d46
vtejaeta <vtejaeta@gmail.com>
2021-03-25 20:19:30 +0530

    Add export for type for movie results

--- src/redux-part/moviesState.ts
+++ src/redux-part/moviesState.ts
@@ -1,41 +1,64 @@
 export interface MoviesState {
-  loading: boolean
-  error: string | null
+  loading: boolean;
+  error: string | null;
   data: {
-    page: number
-    results: {}[]
-    total_pages: number
-    total_results: number
-  } | null
+    page: number;
+    results: MoviesResultState[];
+    total_pages: number;
+    total_results: number;
+  } | null;
 }
+
+export type MoviesResultState = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: number;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
 export const initialState = {
   loading: false,
   error: null,
   data: null,
+};
+
+export interface MovieDetailsState {
+  loading: boolean;
+  error: string | null;
+  data: MovieDetailsResultState | null;
 }
 
-export interface MovieState{
-  loading: boolean
-  error: string | null
-  data:{
-    genres: { id: number; name: string }[];
+export type MovieDetailsResultState = {
+  genres: {
     id: number;
-    original_title: string;
-    overview: string;
-    poster_path: string;
-    release_date: string;
-    runtime: number;
-    videos: {
-      results: {
-        id: string;
-        iso_639_1: string;
-        iso_3166_1: string;
-        key: string;
-        name: string;
-        site: string;
-        size: number;
-        type: string;
-      }[];
-    };
-  } | null
-}
+    name: string;
+  }[];
+  id: number;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  runtime: number;
+  videos: {
+    results: {
+      id: string;
+      iso_639_1: string;
+      iso_3166_1: string;
+      key: string;
+      name: string;
+      site: string;
+      size: number;
+      type: string;
+    }[];
+  };
+};
