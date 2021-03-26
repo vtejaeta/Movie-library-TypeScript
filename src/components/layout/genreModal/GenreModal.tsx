e2797ab6a53b6daa89a284ef7b886f908c5fdb27
vtejaeta <vtejaeta@gmail.com>
2021-03-26 19:48:13 +0530

    Refactor code

--- src/components/layout/genreModal/GenreModal.tsx
+++ src/components/layout/genreModal/GenreModal.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import { Button, Form, Modal } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom'
-
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const Example: React.FC = () => {
-  const [show, setShow] = useState(false)
-  const [genre, setGenre] = useState('action')
-  const history = useHistory()
+  const [show, setShow] = useState(false);
+  const [genre, setGenre] = useState("action");
+
+  const history = useHistory();
 
   function handleClose() {
     return setShow(false);
   }
-  const handleShow = () => setShow(true)
+  const handleShow = () => setShow(true);
 
   return (
     <>
-      <div onClick={handleShow} className='nav-link'>
+      <div onClick={handleShow} className="nav-link">
         Genre
       </div>
       <Modal show={show} onHide={handleClose}>
@@ -23,55 +23,55 @@ const Example: React.FC = () => {
           <Modal.Title>Genre</Modal.Title>
         </Modal.Header>
         <Form
-          className='w-75 p-3 ml-auto mr-auto'
+          className="w-75 p-3 ml-auto mr-auto"
           onSubmit={(e) => {
-            e.preventDefault()
-            history.push(`/browse/${genre}/1`)
+            e.preventDefault();
+            history.push(`/browse/${genre}/1`);
           }}
         >
-          <Form.Group controlId='exampleForm.SelectCustom'>
+          <Form.Group controlId="exampleForm.SelectCustom">
             <Form.Label>Select a genre</Form.Label>
             <Form.Control
-              as='select'
+              as="select"
               value={genre}
               onChange={(e) => {
-                setGenre(e.target.value)
+                setGenre(e.target.value);
               }}
               custom
             >
-              <option value='action'>Action</option>
-              <option value='adventure'>Adventure</option>
-              <option value='animation'>Animation</option>
-              <option value='comedy'>Comedy</option>
-              <option value='crime'>Crime</option>
-              <option value='documentary'>Documentary</option>
-              <option value='drama'>Drama</option>
-              <option value='family'>Family</option>
-              <option value='fantasy'>Fantasy</option>
-              <option value='history'>History</option>
-              <option value='horror'>Horror</option>
-              <option value='music'>Music</option>
-              <option value='mystery'>Mystery</option>
-              <option value='romance'>Romance</option>
-              <option value='science-fiction'>Science Fiction</option>
-              <option value='tv-movie'>TV Movie</option>
-              <option value='thriller'>Thriller</option>
-              <option value='war'>War</option>
-              <option value='western'>Western</option>
+              <option value="action">Action</option>
+              <option value="adventure">Adventure</option>
+              <option value="animation">Animation</option>
+              <option value="comedy">Comedy</option>
+              <option value="crime">Crime</option>
+              <option value="documentary">Documentary</option>
+              <option value="drama">Drama</option>
+              <option value="family">Family</option>
+              <option value="fantasy">Fantasy</option>
+              <option value="history">History</option>
+              <option value="horror">Horror</option>
+              <option value="music">Music</option>
+              <option value="mystery">Mystery</option>
+              <option value="romance">Romance</option>
+              <option value="science-fiction">Science Fiction</option>
+              <option value="tv-movie">TV Movie</option>
+              <option value="thriller">Thriller</option>
+              <option value="war">War</option>
+              <option value="western">Western</option>
             </Form.Control>
           </Form.Group>
           <Modal.Footer>
-            <Button variant='secondary' onClick={handleClose}>
+            <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            <Button variant='primary' type='submit' onClick={handleClose}>
+            <Button variant="primary" type="submit" onClick={handleClose}>
               Submit
             </Button>
           </Modal.Footer>
         </Form>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default Example
+export default Example;
