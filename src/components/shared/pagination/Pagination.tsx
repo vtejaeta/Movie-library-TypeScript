e649ef421c9fae46d1f4cd7b197c175ab80d7210
vtejaeta <vtejaeta@gmail.com>
2021-03-29 19:34:19 +0530

    Attach event listener to button

--- src/components/shared/pagination/Pagination.tsx
+++ src/components/shared/pagination/Pagination.tsx
@@ -1,53 +1,59 @@
-import { useEffect } from 'react'
-import { useHistory, Link } from 'react-router-dom'
-import '../../../assets/css/Pagination.css'
+import { useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import "../../../assets/css/Pagination.css";
 
 interface PaginationProps {
-  totalPages?: number
-  currentPage: number
+  totalPages?: number;
+  currentPage: number;
 }
 
 const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage }) => {
-  const history = useHistory()
+  const history = useHistory();
 
-  const scrollTop = ()=>{
+  const scrollTop = () => {
     window.scrollTo({ top: 0, behavior: "auto" });
-  }
+  };
 
   useEffect(() => {
-      if (totalPages && (!currentPage || currentPage > totalPages)) {
-        history.push('/browse/error')
-      }
-  }, [currentPage, history,totalPages])
+    if (totalPages && (!currentPage || currentPage > totalPages)) {
+      history.push("/browse/error");
+    }
+  }, [currentPage, history, totalPages]);
 
   return (
     <>
       {currentPage === 1 ? (
-        <section className='page-container single-btn' onClick={scrollTop}>
-          <Link to={'2'}>
-            <button className='page-btn float-right'>Page 2</button>
+        <section className="page-container single-btn">
+          <Link to={"2"}>
+            <button className="page-btn float-right" onClick={scrollTop}>
+              Page 2
+            </button>
           </Link>
         </section>
       ) : currentPage === totalPages ? (
-        <section className='page-container single-btn'  onClick={scrollTop}>
+        <section className="page-container single-btn">
           <Link to={`${currentPage - 1}`}>
-            <button className='page-btn float-left'>
+            <button className="page-btn float-left" onClick={scrollTop}>
               Page {totalPages - 1}
             </button>
           </Link>
         </section>
       ) : (
-        <section className='page-container d-flex justify-content-between'  onClick={scrollTop}>
+        <section className="page-container d-flex justify-content-between">
           <Link to={`${currentPage - 1}`}>
-            <button className='page-btn'>Page {currentPage - 1}</button>
+            <button className="page-btn" onClick={scrollTop}>
+              Page {currentPage - 1}
+            </button>
           </Link>
           <Link to={`${currentPage + 1}`}>
-            <button className='page-btn'>Page {currentPage + 1}</button>
+            <button className="page-btn" onClick={scrollTop}>
+              Page {currentPage + 1}
+            </button>
           </Link>
         </section>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
