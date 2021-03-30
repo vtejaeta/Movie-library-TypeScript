44b6d55a1a52e095baa093940daf45a759ca995c
vtejaeta <vtejaeta@gmail.com>
2021-03-30 19:31:04 +0530

    Remove and Refactor code

--- src/screens/displayMovies/DisplayOriginalMovies.tsx
+++ src/screens/displayMovies/DisplayOriginalMovies.tsx
@@ -1,119 +1,121 @@
-import { useEffect, useState } from 'react'
-import { Container, Row } from 'react-bootstrap'
-import { RouteComponentProps } from 'react-router-dom'
-import { useActions } from '../../hooks/useActions'
-import ErrorScreen from '../errorScreen/ErrorScreen'
-import MoviesCarousel from '../../components/layout/carousel/MoviesCarousel'
-import Pagination from '../../components/shared/pagination/Pagination'
-import '../../assets/css/DisplayMovies.css'
-import useAccessMoviesData from '../../hooks/useAccessMoviesData'
-import MoviesGrid from '../../components/layout/moviesGrid/MoviesGrid'
-import { genreArray, genreIdsArray } from '../../utils/genresArray';
+import { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import { RouteComponentProps } from "react-router-dom";
+import { useActions } from "../../hooks/useActions";
+import ErrorScreen from "../errorScreen/ErrorScreen";
+import MoviesCarousel from "../../components/layout/carousel/MoviesCarousel";
+import Pagination from "../../components/shared/pagination/Pagination";
+import "../../assets/css/DisplayMovies.css";
+import useAccessMoviesData from "../../hooks/useAccessMoviesData";
+import MoviesGrid from "../../components/layout/moviesGrid/MoviesGrid";
+import { genreArray, genreIdsArray } from "../../utils/genresArray";
 
 interface MatchParam {
-  category?: string
-  page?: string
+  category?: string;
+  page?: string;
 }
 
 const DisplayOriginalMovies: React.FC<RouteComponentProps<MatchParam>> = ({
   match,
 }) => {
-  const { category } = match.params
-  const [errorParam, setErrorParam] = useState(Number(match.params.page) ? false:true)
+  const { category } = match.params;
+  const [errorParam, setErrorParam] = useState(
+    Number(match.params.page) ? false : true
+  );
   const [currentPage, setCurrentPage] = useState(
-    match.params.page && Number(match.params.page) ? Number(match.params.page) : 1
-  )
+    Number(match.params.page) || 1
+  );
   const {
     searchMoviesByPopular,
     searchMoviesByTopRated,
     searchMoviesByUpComing,
-    searchMoviesByGenreId
-  } = useActions()
+    searchMoviesByGenreId,
+  } = useActions();
 
-  let { loading, error, data } = useAccessMoviesData(category)
+  let { loading, error, data } = useAccessMoviesData(category);
 
   useEffect(() => {
-    match.params.page &&
     Number(match.params.page) &&
-      !Number.isNaN(Number(match.params.page)) &&
       Number(match.params.page) !== currentPage &&
-      setCurrentPage(Number(match.params.page))
+      setCurrentPage(Number(match.params.page));
     if (
-      match.params.page &&
-      !Number.isNaN(Number(match.params.page)) &&
+      Number(match.params.page) > 0 &&
       Number(match.params.page) === currentPage
     ) {
-      if (category === 'popular') {
-        searchMoviesByPopular(currentPage)
-        setErrorParam(false)
-      } else if (category === 'top-rated') {
-        setErrorParam(false)
-        searchMoviesByTopRated(currentPage)
-      } else if (category === 'upcoming') {
-        searchMoviesByUpComing(currentPage)
-        setErrorParam(false)
-      }else if(category && genreArray.includes(category)){
-        let genreId:{id:number,name:String} | undefined;
+      if (category === "popular") {
+        searchMoviesByPopular(currentPage);
+        setErrorParam(false);
+      } else if (category === "top-rated") {
+        setErrorParam(false);
+        searchMoviesByTopRated(currentPage);
+      } else if (category === "upcoming") {
+        searchMoviesByUpComing(currentPage);
+        setErrorParam(false);
+      } else if (category && genreArray.includes(category)) {
+        let genreId: { id: number; name: String } | undefined;
         genreId = genreIdsArray.find((obj) => {
-          return (category === obj.name.replace(' ','-').toLowerCase())
-        })
-        genreId && searchMoviesByGenreId(genreId.id,currentPage)
+          return category === obj.name.replace(" ", "-").toLowerCase();
+        });
+        genreId && searchMoviesByGenreId(genreId.id, currentPage);
       } else {
-        setErrorParam(true)
+        setErrorParam(true);
       }
-    }else if(Number.isNaN(Number(match.params.page))){
-      setErrorParam(true)
+    } else if (
+      Number.isNaN(Number(match.params.page)) ||
+      Number(match.params.page) < 0
+    ) {
+      setErrorParam(true);
     }
     // eslint-disable-next-line
-  }, [category, match, currentPage])
+  }, [category, match, currentPage]);
 
- return (
+  if (errorParam || error) {
+    return <ErrorScreen />;
+  }
+
+  return (
     <>
-      {(errorParam || error) && <ErrorScreen />}
-      {/* {!errorParam && !error && !data && !loading && <LoadingSpinner />} */}
       {loading && (
         <>
-          <MoviesCarousel loadingStatus={'loading'} />
+          <MoviesCarousel loadingStatus={"loading"} />
           <main>
             <Container>
               {category && (
-                <h1 className='ml-4 mt-4'>{category.toUpperCase()} Movies</h1>
+                <h1 className="ml-4 mt-4">{category.toUpperCase()} Movies</h1>
               )}
-              <Row className='p-5'>
-                <MoviesGrid loadingStatus='loading' category={category}/>
+              <Row className="p-5">
+                <MoviesGrid loadingStatus="loading" category={category} />
               </Row>
-              <Pagination
-                totalPages={data?.total_pages}
-                currentPage={currentPage}
-              />
             </Container>
           </main>
         </>
       )}
-      {!error && !loading && data && (
+      {!loading && data && (
         <>
           <MoviesCarousel
             topFourMovies={data?.results.slice(0, 4)}
-            loadingStatus={'loaded'}
+            loadingStatus={"loaded"}
           />
           <main>
             <Container>
               {category && (
-                <h1 className='ml-5 mt-4'>{category.toUpperCase()} Movies</h1>
+                <h1 className="ml-5 mt-4">{category.toUpperCase()} Movies</h1>
               )}
-              <Row className='p-5'>
-                <MoviesGrid loadingStatus='loaded' category={category}/>
+              <Row className="p-5">
+                <MoviesGrid loadingStatus="loaded" category={category} />
               </Row>
-              {(!!data.total_pages) && (data.total_pages !== 1) && <Pagination
-                totalPages={data?.total_pages}
-                currentPage={currentPage}
-              />}
+              {!!data.total_pages && data.total_pages !== 1 && (
+                <Pagination
+                  totalPages={data?.total_pages}
+                  currentPage={currentPage}
+                />
+              )}
             </Container>
           </main>
         </>
       )}
     </>
-  )
-}
+  );
+};
 
-export default DisplayOriginalMovies
+export default DisplayOriginalMovies;
